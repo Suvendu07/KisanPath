786efb4567851441d4259622e2a5c77924c192e3
Sp09 <khuntiasuvendu843@gmail.com>
2026-05-16 07:05:45 +0530

    add vendor table

---
+++ app/models/vendor_model.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, String, Integer, Float, Text, ForeignKey, Boolean, DateTime
+from datetime import datetime
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+
+class Vendor(Base):
+    
+    __tablename__ = "vendors"
+    
+    
+    id = Column(Integer, primary_key=True, index=True)
+    
+    
+    user_id = Column(Integer, ForeignKey("users.id", endelete = "CASCADE"), unique=True, nullable=False)
+    
+    
+    bussiness_name = Column(String(150), nullable=False)
+    bussiness_type = Column(String(100), nullable=True)
+    gst_number = Column(String(20), unique=True, nullable=True)
+    license_number = Column(String(50), unique=True, nullable=True)
+    
+    
+    mandi_name = Column(String(150), nullable=True)
+    mandi_location = Column(String(150), nullable=True)
+    
+    
+    is_approved = Column(Boolean, default=False)
+    bio = Column(Text, nullable=True)
+    
+    
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+ 
+    # ── Relationships ──────────────────────────────────────────────────────────
+    user          = relationship("User",       back_populates="vendor_profile")
+    mandi_prices  = relationship("MandiPrice", back_populates="vendor", cascade="all, delete")
+ 
+    def __repr__(self):
+        return f"<Vendor id={self.id} business={self.business_name}>"
