537b083330350c95b36ef9da5b3de06823742b60
Sp09 <khuntiasuvendu843@gmail.com>
2026-07-17 09:31:29 +0530

    change gemini model

--- app/services/langchain_service.py
+++ app/services/langchain_service.py
@@ -47,7 +47,7 @@ def get_llm() -> ChatGoogleGenerativeAI:
     
     
     return ChatGoogleGenerativeAI(
-        model = "gemini-1.5-flash",
+        model = "gemini-3.1-flash-lite",
         google_api_key = settings.GEMINI_API_KEY,
         temperature = 0.7,
         max_tokens = 1024,
