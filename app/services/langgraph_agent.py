bb7b5cd759b6582d9e5de9f39612959cb9a27d0a
Sp09 <khuntiasuvendu843@gmail.com>
2026-07-17 08:45:51 +0530

    chnage the model in langgrapg agent

--- app/services/langgraph_agent.py
+++ app/services/langgraph_agent.py
@@ -211,7 +211,7 @@ def build_agent_graph():
     
     
     llm = ChatGoogleGenerativeAI(
-        model = "gemini-2.5-flash",
+        model = "gemini-1.5-flash",
         google_api_key = settings.GEMINI_API_KEY,
         temperature = 0.5,
         ).bind_tools(ALL_TOOLS)
